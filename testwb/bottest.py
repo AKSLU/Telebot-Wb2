a913d878c84c1c491397a1f491ffb706d0c5f2e9
AKSLU <152468639+AKSLU@users.noreply.github.com>
2025-06-11 21:50:50 +0500

    Add files via upload

---
+++ testwb/bottest.py
@@ -0,0 +1,40 @@
+import telebot
+from wb import *
+
+token = "7712321312:AAGTEOW2Yfe1vBFe4s1WpdKN4rr8qn_oEwM"
+bot = telebot.TeleBot(token)
+
+whitelist = [6685845705]
+
+def bool_login(chat_id):
+    return chat_id in whitelist 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    if bool_login(message.chat.id):
+        bot.reply_to(message, f"Ваш ID: {message.chat.id}")
+    else:
+        bot.reply_to(message, "No")
+
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    if bool_login(message.chat.id):
+        art = "5007120" 
+        url = "https://basket-01.wbbasket.ru/vol50/part5007/5007120/info/ru/card.json"
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+            "referer": "https://global.wildberries.ru/catalog/5007120/detail.aspx"
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            name = data.get("imt_name")
+            bot.reply_to(message, name)
+        else:
+            bot.reply_to(message, "Товар не найден")
+    else:
+        bot.reply_to(message, "Нет доступа")
+
+
+bot.polling()
